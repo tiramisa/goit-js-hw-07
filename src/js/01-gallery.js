74b994d02fcf39016771599c479b1c3a31cbf8a3
tiramisa <miller19988@gmail.com>
2023-06-05 15:34:20 +0200

    добавила красоту

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const galleryItem = galleryItems[i];
+  const currentImageSource = galleryItem.preview;
+  const item = `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${currentImageSource}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+      />
+    </a>
+  </li>`;
+  gallery.innerHTML += item;
+}
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    const imageSource = event.target.dataset.source;
+
+    const modal = basicLightbox.create(`
+      <img src="${imageSource}" width="800" height="600">
+    `);
+
+    modal.show();
+
+    const closeModalOnEsc = (event) => {
+      if (event.key === "Escape") {
+        modal.close();
+        document.removeEventListener("keydown", closeModalOnEsc);
+      }
+    };
+
+    document.addEventListener("keydown", closeModalOnEsc);
+  }
+});
