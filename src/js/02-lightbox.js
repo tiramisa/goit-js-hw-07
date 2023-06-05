48c0869267089601a98c4b4e935a32ddcf78ca73
tiramisa <miller19988@gmail.com>
2023-06-05 17:56:18 +0200

    вторая часть

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const galleryItem = galleryItems[i];
+  const currentImageSource = galleryItem.preview;
+  const item = `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${currentImageSource}"
+        alt="${galleryItem.description}"
+      />
+    </a>
+  </li>`;
+  gallery.innerHTML += item;
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    lightbox.open();
+  }
+});
